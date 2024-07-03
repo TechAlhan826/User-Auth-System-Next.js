f4796e1bee0fe407a240651ab36d02f6b02e2602
MOHAMMED ALHAN <162118163+TechAlhan826@users.noreply.github.com>
2024-07-04 00:42:38 +0530

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -40,7 +40,7 @@ export default function Signup() {
         } finally {
             setLoading(false);
         }
-        toast("Register Form Submitted !");     
+         
     }
 
     return (
